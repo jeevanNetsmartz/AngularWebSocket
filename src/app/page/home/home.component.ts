2812cb4eb035f81f37173aa2955c1f72b9fd3365
Jeevan Singh <jeevan.s@netsmartz.net>
2022-05-06 19:56:50 +0530

    web socket added
    web socket added

---
+++ src/app/page/home/home.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import * as io from 'socket.io-client';
+import { HomeServiceService } from 'src/app/service/home-service.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  messageText: string = '';
+  messages: Array<any> = [];
+  socket: any;
+  title = 'socketrv';
+  content = '';
+  received = [];
+  sent = [];
+
+  constructor(private WebsocketService: HomeServiceService) {
+    WebsocketService.messages.subscribe(msg => {
+      this.received.push(msg);
+      console.log("Response from websocket: " + msg);
+    });
+  }
+
+  ngOnInit() {
+  }
+
+
+}
